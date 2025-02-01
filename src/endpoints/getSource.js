c12dcd51149fa369eae5b8ac19f7f9a86e5f0968
Muhammet Derviş Aygan <zfcsoftware@gmail.com>
2025-02-02 00:11:11 +0300

    fix: solve the proxy problem
    https://pptr.dev/api/puppeteer.browsercontextoptions

--- src/endpoints/getSource.js
+++ src/endpoints/getSource.js
@@ -1,60 +1,60 @@
 function getSource({ url, proxy }) {
-    return new Promise(async (resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    if (!url) return reject("Missing url parameter");
+    const context = await global.browser
+      .createBrowserContext({
+        proxyServer: proxy ? `http://${proxy.host}:${proxy.port}` : undefined, // https://pptr.dev/api/puppeteer.browsercontextoptions
+      })
+      .catch(() => null);
+    if (!context) return reject("Failed to create browser context");
 
-        if (!url) return reject('Missing url parameter')
-        const context = await global.browser.createBrowserContext().catch(() => null);
-        if (!context) return reject('Failed to create browser context')
+    let isResolved = false;
 
-        let isResolved = false
+    var cl = setTimeout(async () => {
+      if (!isResolved) {
+        await context.close();
+        reject("Timeout Error");
+      }
+    }, global.timeOut || 60000);
 
-        const { proxyRequest } = await import('puppeteer-proxy')
+    try {
+      const page = await context.newPage();
 
-        var cl = setTimeout(async () => {
-            if (!isResolved) {
-                await context.close()
-                reject("Timeout Error")
-            }
-        }, (global.timeOut || 60000))
+      if (proxy?.username && proxy?.password)
+        await page.authenticate({
+          username: proxy.username,
+          password: proxy.password,
+        });
 
+      await page.setRequestInterception(true);
+      page.on("request", async (request) => request.continue());
+      page.on("response", async (res) => {
         try {
-            const page = await context.newPage();
-            await page.setRequestInterception(true);
-            page.on('request', async (request) => {
-                try {
-                    if (proxy) {
-                        await proxyRequest({
-                            page,
-                            proxyUrl: `http://${proxy.username ? `${proxy.username}:${proxy.password}@` : ""}${proxy.host}:${proxy.port}`,
-                            request,
-                        });
-                    } else {
-                        request.continue()
-                    }
-                } catch (e) { }
-            });
-            page.on('response', async (res) => {
-                try {
-                    if ([200, 302].includes(res.status()) && [url, url + '/'].includes(res.url())) {
-                        await page.waitForNavigation({ waitUntil: 'load', timeout: 5000 }).catch(() => { });
-                        const html = await page.content();
-                        await context.close()
-                        isResolved = true
-                        clearInterval(cl)
-                        resolve(html)
-                    }
-                } catch (e) { }
-            })
-            await page.goto(url, {
-                waitUntil: 'domcontentloaded'
-            })
-        } catch (e) {
-            if (!isResolved) {
-                await context.close()
-                clearInterval(cl)
-                reject(e.message)
-            }
-        }
-
-    })
+          if (
+            [200, 302].includes(res.status()) &&
+            [url, url + "/"].includes(res.url())
+          ) {
+            await page
+              .waitForNavigation({ waitUntil: "load", timeout: 5000 })
+              .catch(() => {});
+            const html = await page.content();
+            await context.close();
+            isResolved = true;
+            clearInterval(cl);
+            resolve(html);
+          }
+        } catch (e) {}
+      });
+      await page.goto(url, {
+        waitUntil: "domcontentloaded",
+      });
+    } catch (e) {
+      if (!isResolved) {
+        await context.close();
+        clearInterval(cl);
+        reject(e.message);
+      }
+    }
+  });
 }
-module.exports = getSource
+module.exports = getSource;
--- src/endpoints/solveTurnstile.max.js
+++ src/endpoints/solveTurnstile.max.js
@@ -1,82 +1,81 @@
 function solveTurnstileMin({ url, proxy }) {
-    return new Promise(async (resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    if (!url) return reject("Missing url parameter");
 
-        if (!url) return reject('Missing url parameter')
+    const context = await global.browser
+      .createBrowserContext({
+        proxyServer: proxy ? `http://${proxy.host}:${proxy.port}` : undefined, // https://pptr.dev/api/puppeteer.browsercontextoptions
+      })
+      .catch(() => null);
 
-        const context = await global.browser.createBrowserContext().catch(() => null);
+    if (!context) return reject("Failed to create browser context");
 
-        if (!context) return reject('Failed to create browser context')
+    let isResolved = false;
+    const { RequestInterceptionManager } = await import(
+      "puppeteer-intercept-and-modify-requests"
+    );
 
-        let isResolved = false
-        const { proxyRequest } = await import('puppeteer-proxy')
-        const { RequestInterceptionManager } = await import('puppeteer-intercept-and-modify-requests')
+    var cl = setTimeout(async () => {
+      if (!isResolved) {
+        await context.close();
+        reject("Timeout Error");
+      }
+    }, global.timeOut || 60000);
 
-        var cl = setTimeout(async () => {
-            if (!isResolved) {
-                await context.close()
-                reject("Timeout Error")
-            }
-        }, (global.timeOut || 60000))
+    try {
+      const page = await context.newPage();
 
+      if (proxy?.username && proxy?.password)
+        await page.authenticate({
+          username: proxy.username,
+          password: proxy.password,
+        });
+
+      const client = await page.target().createCDPSession();
+      const interceptManager = new RequestInterceptionManager(client);
+
+      await page.setRequestInterception(true);
+      page.on("request", async (request) => request.continue());
+
+      await interceptManager.intercept({
+        urlPattern: url,
+        resourceType: "Document",
+        modifyResponse({ body }) {
+          return {
+            body: String(body).replace(
+              "</body>",
+              String(require("fs").readFileSync("./src/data/callback.html")) +
+                "</body>"
+            ),
+          };
+        },
+      });
+      await page.goto(url, {
+        waitUntil: "domcontentloaded",
+      });
+
+      await page.waitForSelector('[name="cf-response"]', {
+        timeout: 60000,
+      });
+      const token = await page.evaluate(() => {
         try {
-            const page = await context.newPage();
-
-            const client = await page.target().createCDPSession()
-            const interceptManager = new RequestInterceptionManager(client)
-
-            await page.setRequestInterception(true);
-            page.on('request', async (request) => {
-                try {
-                    if (proxy) {
-                        await proxyRequest({
-                            page,
-                            proxyUrl: `http://${proxy.username ? `${proxy.username}:${proxy.password}@` : ""}${proxy.host}:${proxy.port}`,
-                            request,
-                        });
-                    } else {
-                        request.continue()
-                    }
-                } catch (e) { }
-            });
-
-            await interceptManager.intercept(
-                {
-                    urlPattern: url,
-                    resourceType: 'Document',
-                    modifyResponse({ body }) {
-                        return {
-                            body: String(body).replace('</body>', String(require('fs').readFileSync('./src/data/callback.html')) + '</body>')
-                        }
-                    },
-                }
-            )
-            await page.goto(url, {
-                waitUntil: 'domcontentloaded'
-            })
-
-            await page.waitForSelector('[name="cf-response"]', {
-                timeout: 60000
-            })
-            const token = await page.evaluate(() => {
-                try {
-                    return document.querySelector('[name="cf-response"]').value
-                } catch (e) {
-                    return null
-                }
-            })
-            isResolved = true
-            clearInterval(cl)
-            await context.close()
-            if (!token || token.length < 10) return reject('Failed to get token')
-            return resolve(token)
+          return document.querySelector('[name="cf-response"]').value;
         } catch (e) {
-            if (!isResolved) {
-                await context.close()
-                clearInterval(cl)
-                reject(e.message)
-            }
+          return null;
         }
-
-    })
+      });
+      isResolved = true;
+      clearInterval(cl);
+      await context.close();
+      if (!token || token.length < 10) return reject("Failed to get token");
+      return resolve(token);
+    } catch (e) {
+      if (!isResolved) {
+        await context.close();
+        clearInterval(cl);
+        reject(e.message);
+      }
+    }
+  });
 }
-module.exports = solveTurnstileMin
+module.exports = solveTurnstileMin;
